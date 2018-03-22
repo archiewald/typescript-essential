9b764d7b229b50488810f435031718408a67a747
archiewald <artur.kozubek1@gmail.com>
2018-03-22 19:04:52 +0100

    - let and const - for..of loop

--- TypeScript-todo/app.js
+++ TypeScript-todo/app.js
@@ -6,4 +6,22 @@ var todo = {
     completed: true
 };
 container.innerHTML = "\n<div todo='" + todo.id + "' class=\"list-group-item}\">\n    <i class=\"" + (todo.completed ? "" : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n    <span class=\"name\">" + todo.name + "</span>\n</div>\n";
+var container = document.getElementById('container');
+// let and const
+for (var x = 0; x <= 5; x++) {
+    var counter = x;
+    //counter = 1;
+}
+// console.log(counter); // not accessible!
+// for...of loop
+var array = [
+    "Pick up drycleaning",
+    "Clean Batcave",
+    "Save Gotham"
+];
+for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
+    var value = array_1[_i];
+    console.log(value);
+}
+array.map(function (x) { return console.log(x); });
 //# sourceMappingURL=app.js.map
--- TypeScript-todo/app.ts
+++ TypeScript-todo/app.ts
@@ -14,3 +14,28 @@ container.innerHTML = `
     <span class="name">${todo.name}</span>
 </div>
 `
+
+var container = document.getElementById('container');
+
+// let and const
+
+for (var x = 0; x <= 5; x++) {
+    let counter = x;
+    //counter = 1;
+}
+// console.log(counter); // not accessible!
+
+// for...of loop
+
+var array = [
+    "Pick up drycleaning", 
+    "Clean Batcave", 
+    "Save Gotham"
+];
+
+for (var value of array) {
+    console.log(value);
+}
+
+array.map( x => console.log(x))
+
