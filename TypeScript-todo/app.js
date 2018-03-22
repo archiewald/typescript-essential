22a1e92520fbeb53681e2849b0056eef489fbdac
archiewald <artur.kozubek1@gmail.com>
2018-03-22 18:42:05 +0100

    Template literals

--- TypeScript-todo/app.js
+++ TypeScript-todo/app.js
@@ -1,4 +1,9 @@
-var foo = function () {
-    console.log("something");
+var container = document.getElementById('container');
+// using typescript literals
+var todo = {
+    id: 123,
+    name: 'Pick up drycleaning',
+    completed: true
 };
+container.innerHTML = "\n<div todo='" + todo.id + "' class=\"list-group-item}\">\n    <i class=\"" + (todo.completed ? "" : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n    <span class=\"name\">" + todo.name + "</span>\n</div>\n";
 //# sourceMappingURL=app.js.map
--- TypeScript-todo/app.ts
+++ TypeScript-todo/app.ts
@@ -1,3 +1,16 @@
-const foo = () => {
-    console.log("something");
+var container = document.getElementById('container');
+
+// using typescript literals
+
+var todo = {
+    id: 123,
+    name: 'Pick up drycleaning',
+    completed: true
 }
+
+container.innerHTML = `
+<div todo='${todo.id}' class="list-group-item}">
+    <i class="${ todo.completed ? "" : "hidden" } text-success glyphicon glyphicon-ok"></i>
+    <span class="name">${todo.name}</span>
+</div>
+`
