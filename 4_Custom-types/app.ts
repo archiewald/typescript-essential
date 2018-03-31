afbf31c0bd3fe49ce4c435a454d04f908dd8f221
archiewald <artur.kozubek1@gmail.com>
2018-03-31 18:29:42 +0200

    interfaces

---
+++ 4_Custom-types/app.ts
@@ -0,0 +1,40 @@
+console.log("Custom types");
+{
+    interface Todo {
+        name: string; // types are optional within interface
+        completed: boolean;    
+        //completed?: boolean; // can be marked as optional with ?   
+    }
+
+    // interfaces are used only for compiled check. There is no representation in app.js!
+
+    const todo: Todo = {
+        name: "Pranko",
+        completed: false,
+    }
+
+    // const todo = <Todo> {};
+    // ^casting syntax!
+
+    interface ITodoService {
+        add(todo: Todo): Todo;
+        delete(todo: Todo): void;
+        getAll(): Todo[];
+        getById(todoId: number): Todo;
+    }
+
+    interface jQuery {
+        (selector: string): HTMLElement;
+        version: number;
+    }
+    
+    // using interface to describe functions
+    
+    var $ = <jQuery>function(selector: string) {
+        // Find DOM element
+    }
+    
+    $.version = 1.18
+    
+    var container = $("#container");
+}
